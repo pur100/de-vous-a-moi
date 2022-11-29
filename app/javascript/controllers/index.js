1d4e13b4a7af980dbcc7eed200ade847be3bc885
Romain Heranney <heranney.romain@gmail.com>
2022-11-29 18:07:12 +0100

    Svg -controller stimulus pattern -canvas et image tag dans la show -yarn fabric -gem inline-svg

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import PatternController from "./pattern_controller"
+application.register("pattern", PatternController)
