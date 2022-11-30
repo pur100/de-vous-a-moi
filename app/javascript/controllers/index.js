38392c7cddce2f824e9f7dc9ffd118cd092e1b84
Romain Heranney <heranney.romain@gmail.com>
2022-11-30 14:31:54 +0100

    conflits a veniirr

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-// import PatternController from "./pattern_controller"
-// application.register("pattern", PatternController)
+import PatternController from "./pattern_controller"
+application.register("pattern", PatternController)
--- app/javascript/controllers/pattern_controller.js
+++ app/javascript/controllers/pattern_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 import { fabric } from 'fabric'
 
+
+// RORO SANDBOX
 export default class extends Controller {
+  static targets = ["shape"];
+
   connect() {
     console.log("connecté");
   }
@@ -9,18 +13,59 @@ export default class extends Controller {
   addAShape() {
     console.log("dans addAShape");
     const canvas = new fabric.Canvas('canvas');
-    console.log(canvas);
-    // const imgElement = document.getElementById('test-shape');
-    // console.log(imgElement);
-    // let imgInstance = new fabric.loadSVGFromString();
-    canvas.add(imgInstance);
+    let group = [];
+
+    // console.log(this.shapeTarget.innerHTML);
+    fabric.loadSVGFromString(this.shapeTarget.innerHTML, function(objects,options) {
+      let loadedObjects = new fabric.Group(group);
+      loadedObjects.set({
+          width: 200,
+          height: 200
+      });
+      console.log(loadedObjects);
+      canvas.centerObject(loadedObjects);
+      canvas.add(loadedObjects);
+      canvas.renderAll();
+      },function(item, object) {
+        object.set('id',item.getAttribute('id'));
+        group.push(object);
+    });
   }
 }
-//  const rect = new fabric.Rect({
-//       top: 100,
-//       left: 100,
-//       width: 60,
-//       height: 70,
-//       fill: 'red',
-//     });
-//     canvas.add(rect);
+  // CACA
+  // console.log(canvas);
+  // // const imgElement = document.getElementById('test-shape');
+  // // console.log(imgElement);
+  // // let imgInstance = new fabric.loadSVGFromString();
+  // canvas.add(imgInstance);
+
+  // METTRE UN RECTANGLE ROUGE
+  // const rect = new fabric.Rect({
+    //   top: 100,
+    //   left: 100,
+    //   width: 60,
+    //   height: 70,
+    //   fill: 'red',
+    // });
+
+
+    //   SVG NOIR INNACCESSIBLE
+    //   console.log("dans addAShape");
+    //   const canvas = new fabric.Canvas('canvas');
+    //   let group = [];
+
+    //   // console.log(this.shapeTarget.innerHTML);
+    //   fabric.loadSVGFromString(this.shapeTarget.innerHTML, function(objects,options) {
+    //   let loadedObjects = new fabric.Group(group);
+    //   loadedObjects.set({
+    //       width: 200,
+    //       height: 200
+    //   });
+    //   canvas.centerObject(loadedObjects);
+    //   canvas.add(loadedObjects);
+    //   canvas.renderAll();
+    //   },function(item, object) {
+    //     object.set('id',item.getAttribute('id'));
+    //     group.push(object);
+    //   });
+    // }
