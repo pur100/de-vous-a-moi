84395f418fc9773c1f82badf3949435e5645c3f3
Romain Heranney <heranney.romain@gmail.com>
2022-12-01 14:38:08 +0100

    added a list of buttons with hexadecimal colors text and changing shapes with the selected color

--- app/javascript/controllers/pattern_controller.js
+++ app/javascript/controllers/pattern_controller.js
@@ -3,85 +3,38 @@ import { fabric } from 'fabric'
 
 export default class extends Controller {
 static targets = ["shape", "shapeForColoring"];
+// static values = { color: String }
 
   connect() {
     console.log("connecté");
+    // on crée un canvas de travail pour fabric dans le canvas HTML et on en fait une variable d'instance
     this.canvas = new fabric.Canvas('canvas');
   }
 
-
   addAShape() {
-   // on crée un canvas de travail pour fabric dans le canvas HTML
-    // on donne à l'élément canvas HTML la valeur de notre nouveau canvas (de la sorte, canvas "fabric" est accesible partout, au même titre que cavas "html")
-    // document.getElementById("canvas").fabric = canvas;
     // on load le svg souhaité (en cliquant sur l'image du svg disponible sur la page)
     fabric.loadSVGFromString(this.shapeTarget.innerHTML, this.#loadSVG.bind(this))
   }
+
   // methode pour changer la couleur de la forme sélectionnée
-  changeShapeColor() {
-    // const canvas = Canvas.where(this.shapeForColoringTarget);
-    // const canvas = document.getElementById("canvas").fabric;
-    console.log(this.canvas.getActiveObject());
+  changeShapeColor(event) {
     let activ_object = this.canvas.getActiveObject();
+    let color = event.target.innerHTML;
     activ_object._objects.forEach(object => {
-      object.set({ fill: '#DA0D58' });
+      object.set({ fill: color });
     });
     this.canvas.renderAll();
   }
 
+  // private methode pour load le svg, en faire un groupe d'éléments, le sizer et le centrer
   #loadSVG(objects, options) {
-    let obj = fabric.util.groupSVGElements(objects, options);
-    this.canvas.add(obj).renderAll();
-    obj.scaleToHeight(this.canvas.height/2); // Scales it down to half the size of the canvas
-    obj.scaleToWidth(this.canvas.width/2); // Scales it down to half the size of the canvas
-    obj.center();
+    this.obj = fabric.util.groupSVGElements(objects, options);
+    this.canvas.add(this.obj).renderAll();
+    this.obj.scaleToHeight(this.canvas.height/2); // Scales it down to half the size of the canvas
+    this.obj.scaleToWidth(this.canvas.width/2); // Scales it down to half the size of the canvas
+    this.obj.center();
   };
 }
-
-// FIN RORO SANDBOX CHANGE COLOR
-
-// DEBUT FABRIZIO SANDBOX
-    // On charge le svg dans le canvas
-  //   fabric.loadSVGFromString(svg, function(objects, options) {
-  //     let obj = fabric.util.groupSVGElements(objects, options);
-
-  //     canvas.add(obj).renderAll();
-  //     obj.scaleToHeight(canvas.height/2)
-  //       obj.center();
-
-  //     // Change color of the svg just after adding it to the canvas
-  //     const color = '#D556C4';
-  //     if (obj && obj._objects) {
-  //       for (var i = 0; i < obj._objects.length; i++) {
-  //         obj._objects[i].set({
-  //           fill: color
-  //         });
-  //       }
-  //     }
-  //   });
-
-  //   // On sélectionne l'objet quand on clique dessus
-  //   canvas.on({
-  //     'selection:updated': HandleElement,
-  //     'selection:created': HandleElement
-  //   });
-
-  //   // Et on lui change sa couleur
-  //   function HandleElement(obj){
-  //      //Handle the object here
-  //     // console.log(obj.selected[0]._objects)
-  //     const color2 = '#0000FF'
-  //     for (var i = 0; i < obj.selected[0]._objects.length; i++) {
-  //       // console.log(obj.selected[0]._objects[i])
-  //       // obj.selected[0]._objects[i].fill = color2
-  //       obj.selected[0]._objects[i].set({
-  //         fill: color2
-  //       });
-  //     }
-  //   }
-  // }
-// FIN FABRIZIO SANDBOX
-
 
 // METHODE NESTEE DANS ADD A SHAPE
 // const testMethod = () => {
